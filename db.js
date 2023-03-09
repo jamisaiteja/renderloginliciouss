26970b89c2a95b0083eb32f728f71ea424cc910b
jamisaiteja <jamisaiteja666@gmail.com>
2023-03-09 12:15:56 +0530

    commit

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 // database connection
 
 let mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jami:Venkatsai@cluster0.aiz67sc.mongodb.net/?retryWrites=true&w=majority/liciousdata')
+mongoose.connect('mongodb+srv://jami:Venkatsai@cluster0.aiz67sc.mongodb.net/?retryWrites=true&w=majority/?liciousdata')
